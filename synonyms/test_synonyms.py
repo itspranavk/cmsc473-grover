f6d685d7930fd23b762189dc35b12a3b06f8a1d8
Kevin-G-Nolan <53273651+Kevin-G-Nolan@users.noreply.github.com>
2021-12-19 19:28:53 -0500

    Update test_synonyms.py

--- synonyms/test_synonyms.py
+++ synonyms/test_synonyms.py
@@ -1,41 +1,33 @@
-# A program that tests the output of Grover when words are changed to their synonyms
+# A program that substitues words for their sysnonyms in a news article
 
 import os
 import sys
 import json
 import random
+import nltk
+
+from nltk.corpus import wordnet
 
 # Get articles
-input_unmodified_path = sys.argv[1]
-input_modified_path = sys.argv[2]
-output_path = sys.argv[3]
+input_path = sys.argv[1]
+output_path = sys.argv[2]
 
-unmodified_articles = open(input_unmodified_path, "r");
-modified_articles = open(input_modified_path, "r");
+input = open(input_path, "r");
 
-unmodified = []
-modified = []
+articles = []
 
-for line in unmodified_articles:
+for line in input:
     item = json.loads(line)
-    unmodified.append(item)
+    articles.append(item)
 
-for line in modified_articles:
-    item = json.loads(line)
-    modified.append(item)
-
-unmodified_articles.close()
-modified_articles.close()
+input.close()
 
 # Generate articles
 output_file = open(output_path, "w+")
 
-for i in range(0, len(unmodified)):
-    article = unmodified[i]["text"]
-    syn_article = modified[i]["text"]
-
+for i in range(0, len(articles)):
+    article = articles[i]["text"]
     words = article.split()
-    syns = syn_article.split()
 
     idxs = list(range(0, len(words)))
     random.shuffle(idxs)
@@ -43,15 +35,18 @@ for i in range(0, len(unmodified)):
     num = 1;
     
     for j in idxs:
-        words[j] = syns[j]
+        syn = wordnet.synsets(words[j])[0].lemmas()[0].name()
+        
+        if (wordnet.synset(words[j]).wup_similarity(wordnet.synset(syn)) > 0.55):
+            words[j] = syn
 
         new_article = ' '.join(words)
-        unmodified[i]["text"] = new_article
+        articles[i]["text"] = new_article
 
-        unmodified[i]["authors"] = "[]"
-        unmodified[i]["split"] = "test"
-        unmodified[i]["label"] = ""
-        unmodified[i]["numSyns"] = num
+        articles[i]["authors"] = "[]"
+        articles[i]["split"] = "test"
+        articles[i]["label"] = ""
+        articles[i]["numSyns"] = num
         
         num++
 
